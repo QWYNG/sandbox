f749b303a34a278c8ea93e5df0ee3a80a0964111
QWYNG <ikusawasi@gmail.com>
2024-11-14 21:31:29 +0900

    リーフはいい感じ

--- configration_parser.rb
+++ configration_parser.rb
@@ -13,18 +13,22 @@ class StackDiagram
     @stack = stack
   end
 
-  def generate_svg(file_name = "stack_diagram.svg")
-    svg = Victor::SVG.new(width: 200, height: 50 * @stack.size + 100)
-    svg.rect(x: 0, y: 0, width: 200, height: 50 * @stack.size + 100, fill: 'lightgray', stroke: 'black')
-    svg.text(@name, x: 100, y: 25, 'text-anchor' => 'middle', 'font-size' => 30)
+  def generate_svg(x, y)
+    svg = Victor::SVG.new
+    svg.rect(x: x, y: y, width: 400, height: 50 * @stack.size + 100, fill: 'lightgray', stroke: 'black')
+    svg.text(@name, x: x + 100, y: y + 25, 'text-anchor' => 'middle', 'font-size' => 30)
 
     @stack.each_with_index do |element, index|
-      y_position = (index) * 50 + 100
-      svg.rect(x: 10, y: y_position, width: 180, height: 40, fill: 'lightblue', stroke: 'black')
-      svg.text(element.to_s, x: 100, y: y_position + 25, 'text-anchor' => 'middle', 'font-size' => 20)
+      y_position = y + (index) * 50 + 50
+      svg.rect(x: x + 10, y: y_position, width: 180, height: 40, fill: 'lightblue', stroke: 'black')
+      svg.text(element.to_s, x: x + 100, y: y_position + 20, 'text-anchor' => 'middle', 'font-size' => 20)
     end
 
-    svg.save(file_name)
+    svg
+  end
+
+  def size
+    @stack.size
   end
 end
 
@@ -34,34 +38,45 @@ class MapDiagram
     @map = map
   end
 
-  def generate_svg(file_name = "map_diagram.svg")
-    svg = Victor::SVG.new(width: 200, height: 50 * @map.size + 100)
-    svg.rect(x: 0, y: 0, width: 200, height: 50 * @map.size + 100, fill: 'lightgray', stroke: 'black')
-    svg.text(@name, x: 100, y: 25, 'text-anchor' => 'middle', 'font-size' => 30)
+  def generate_svg(x, y)
+    svg = Victor::SVG.new
+    svg.rect(x: x, y: y, width: 400, height: 50 * @map.size + 100, fill: 'lightgray', stroke: 'black')
+    svg.text(@name, x: x + 100, y: y + 25, 'text-anchor' => 'middle', 'font-size' => 30)
 
     @map.each_with_index do |(key, value), index|
-      y_position = (index) * 50 + 100
-      svg.rect(x: 10, y: y_position, width: 180, height: 40, fill: 'lightgreen', stroke: 'black')
-      svg.text("#{key} => #{value}", x: 100, y: y_position + 25, 'text-anchor' => 'middle', 'font-size' => 20)
+      y_position = y + (index) * 50 + 50
+      svg.rect(x: x + 10, y: y_position, width: 180, height: 40, fill: 'lightgreen', stroke: 'black')
+      svg.text("#{key} -> #{value}", x: x + 100, y: y_position + 25, 'text-anchor' => 'middle', 'font-size' => 20)
     end
 
-    svg.save(file_name)
+    svg
+  end
+
+  def size
+    @map.size
   end
 end
 
 class Diagram
   def initialize(name)
     @name = name
-    @svg = Victor::SVG.new(width: 1000, height: 1000)
-    @current_x = 0
-    @current_y = 0
+    @svg = Victor::SVG.new(width: '100000px', height: '100000px')
+    @current_x = 1
+    @current_y = 1
   end
 
   def add_map(map_diagram)
-    
+    map_svg = map_diagram.generate_svg(@current_x, @current_y)
+    @svg.append(map_svg)
+    @current_x += 0
+    @current_y += 50 * map_diagram.size + 100
   end
 
-  def add_stack(element)
+  def add_stack(stack_diagram)
+    stack_svg = stack_diagram.generate_svg(@current_x, @current_y)
+    @svg.append(stack_svg)
+    @current_x += 0
+    @current_y += 50 * stack_diagram.size + 100
   end
 
   def save(file_name = "combined_diagram.svg")
@@ -103,15 +118,17 @@ class PrintConfiguration
   end
 
   def print
-    print_element(@config.tree)
+    tree = @config.tree
+    diagram = Diagram.new(tree.name)
+    print_element(@config.tree, diagram)
   end
 
-  def print_element(element)
+  def print_element(element, diagram)
     case element.sort
     when :map
-      MapDiagram.new(element.name, {}).generate_svg
+      diagram.add_map(MapDiagram.new(element.name, {1 => 2, 3 => 4}))
     when :list
-      StackDiagram.new(element.name, []).generate_svg
+      diagram.add_stack(StackDiagram.new(element.name, [:a, :b, :c]))
     when :edge
       puts "Edge: #{element.name}"
     else
@@ -119,7 +136,11 @@ class PrintConfiguration
     end
 
     element.each_element do |child|
-      print_element(child)
+      print_element(child, diagram)
+    end
+
+    if element.parent.kind_of?(REXML::Document)
+      diagram.save("#{element.name}.svg")
     end
   end
 end
